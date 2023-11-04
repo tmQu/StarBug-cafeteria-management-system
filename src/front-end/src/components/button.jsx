e6ee4a1c17b05ef406a2d060cce46207869dcb4e
HuyBean <nqhuy0711@gmail.com>
2023-11-04 19:12:33 +0700

    Huy code button

--- src/front-end/src/components/button.jsx
+++ src/front-end/src/components/button.jsx
@@ -1,4 +1,17 @@
-const Button = () => {
-    // write reusable button to call to use in navigation bar and "Watch all product"
-  return <div></div>;
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button
+      type="button"
+      className={`px-2 text-sm bg-red-600 text-white rounded font-light border-none transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-xl ${
+        props.isClicked ? "text-black bg-white" : ""
+      }`}
+      onClick={props.onClick}
+    >
+      {props.name}
+    </button>
+  );
 };
+
+export default Button;
--- src/front-end/src/components/header.jsx
+++ src/front-end/src/components/header.jsx
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
+import Button from "./button";
+
 const Header = () => {
-  // call search bar and some reusable button for login
-  // create circle button for cart icon
+  const [selectedButton, setSelectedButton] = useState("Home");
+
+  const handleButtonClick = (name) => {
+    setSelectedButton(name);
+  };
+
   return (
-    <div className="w-screen px-[10vw] h-full grid-in-header bg-slate-300">Header</div>
+    <div className="w-screen h-full grid-in-header bg-red-600">
+      Header
+      <div className="flex space-x-5 mx-auto">
+        {["Home", "Coffee", "Tea", "Milktea", "Cake", "About"].map((name) => (
+          <Button
+            key={name}
+            name={name}
+            isClicked={name === selectedButton}
+            onClick={() => handleButtonClick(name)}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
+
 export default Header;
