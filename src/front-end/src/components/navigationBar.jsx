01e14b8b5328d379513bea4cd489e9761d60eb18
HuyBean <nqhuy0711@gmail.com>
2023-11-05 11:09:38 +0700

    commit

--- src/front-end/src/components/navigationBar.jsx
+++ src/front-end/src/components/navigationBar.jsx
@@ -3,29 +3,6 @@ import { Link } from "react-router-dom";
 import Button from "./button";
 
 const NavigationBar = () => {
-<<<<<<< HEAD
-  const [selectedButton, setSelectedButton] = useState("Home");
-
-  const handleButtonClick = (name) => {
-    setSelectedButton(name);
-  };
-  
-  const categories = ["Home", "About", "Coffee", "Tea", "MilkTea","Cake"];
-
-  return (
-    <div className="w-screen h-full px-[10vw] grid-in-navigation bg-[#D63431]">
-      <div className="flex flex-row space-x-2 pt-1">
-        {categories.map((name) => (
-          <Link to={"/" + name.toLowerCase()} key={name}>
-            <Button
-              name={name}
-              isClicked={name === selectedButton}
-              onClick={() => handleButtonClick(name)}
-            />
-          </Link>
-        ))}
-      </div>
-=======
   const routes = [
     { name: "Home", path: "/" },
     { name: "Tea", path: "/tea" },
@@ -46,7 +23,6 @@ const NavigationBar = () => {
           </li>
         ))}
       </ul>
->>>>>>> origin/HEAD
     </div>
   );
 };
