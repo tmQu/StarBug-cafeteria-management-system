1ff262c02732393730c1a2d05261bfbfe03f2e60
HuyBean <nqhuy0711@gmail.com>
2023-11-08 16:20:07 +0700

    Huy-merge-branch-HuyBean-Chuong

--- src/front-end/src/pages/about.jsx
+++ src/front-end/src/pages/about.jsx
@@ -37,8 +37,8 @@ const About = () => {
     },
   ];
   return (
-    <div>
-      <div className="w-screen h-fit px-[10vw] bg-orange-300">
+    <div className=" bg-[#F4F2EC]">
+      <div className="w-screen h-fit px-[10vw]">
         <Story image={`https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg`} title='The Perfect Brew' subtitle='Coffee' paragraph={`Are you ready to embark on a journey of taste and aroma like no other? We are thrilled to introduce our latest coffee creation, a true masterpiece for coffee enthusiasts. At the heart of this exceptional brew lies the perfect blend of premium beans from the world's most renowned `} />
       </div>
       <div className="w-screen h-fit px-[10vw] p-8">
