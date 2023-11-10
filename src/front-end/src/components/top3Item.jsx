b511e24b415a9e3280acd0fe4d974801fee31276
DinhChuong2909 <vudinhchuong2909@gmail.com>
2023-11-10 08:12:36 +0700

    Chuong - Mini fix top3Item component

--- src/front-end/src/components/top3Item.jsx
+++ src/front-end/src/components/top3Item.jsx
@@ -2,7 +2,7 @@ const Top3Item = (props) => {
   const { id, name, price, feature, image } = props;
   return (
     <div
-      className="w-[300px] h-[385px] bg-[#D8D4BA] rounded-xl shadow-xl
+      className="group w-[300px] h-[385px] bg-[#D8D4BA] rounded-xl shadow-xl
     [transition:transform_0.5s_ease] hover:scale-[1.02] hover:bg-[#0D3745]"
     >
       <div className="pt-3">
@@ -14,10 +14,10 @@ const Top3Item = (props) => {
         ></img>
       </div>
       <div className="flex flex-col items-center mt-1.5 hover:cursor-pointer">
-        <p className="text-xl font-light text-[#0D3745]">{feature}</p>
-        <hr className="w-4/5 h-[1px] rounded mx-auto border-none bg-[#0D3745]"></hr>
-        <p className="text-3xl font-light mt-1 text-[#0D3745]">{name}</p>
-        <p className="text-2xl font-normal -mt-1 text-[#0D3745]">{price}</p>
+        <p className="text-xl font-light text-[#0D3745] group-hover:text-[#F3F2ED]">{feature}</p>
+        <hr className="w-4/5 h-[1px] rounded mx-auto border-none bg-[#0D3745] group-hover:bg-[#F3F2ED]"></hr>
+        <p className="text-3xl font-light mt-1 text-[#0D3745] group-hover:text-[#F3F2ED]">{name}</p>
+        <p className="text-2xl font-normal -mt-1 text-[#0D3745] group-hover:text-[#F3F2ED]">{price}</p>
       </div>
     </div>
   );
