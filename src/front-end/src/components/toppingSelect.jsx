66cc30f14379ffbda9997d065a7523d016bb0dd2
HuyBean <nqhuy0711@gmail.com>
2023-11-18 14:08:33 +0700

    Huy build topping, selectSize, selectTopping component

---
+++ src/front-end/src/components/toppingSelect.jsx
@@ -0,0 +1,35 @@
+import Topping from "./topping";
+
+const ToppingSelect = () => {
+
+    return (
+        <div className="w-400">
+            <div className="py-1 bg-[#D9D9D9] h-[40px] text-[#666] font-Source-Sans-3 text-[20px] font-norma pl-3">
+                Topping
+            </div>
+            <div className="flex flex-col justify-center items-center bg-[#F3F2ED]">
+                <Topping />
+                <div className="w-[340px] stroke-[1px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="342" height="2" viewBox="0 0 342 2" fill="none">
+                        <path d="M1 1H341" stroke="#D9D9D9" stroke-linecap="round" />
+                    </svg>
+                </div>
+                <Topping />
+                <div className="w-[340px] stroke-[1px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="342" height="2" viewBox="0 0 342 2" fill="none">
+                        <path d="M1 1H341" stroke="#D9D9D9" stroke-linecap="round" />
+                    </svg>
+                </div>
+                <Topping />
+                <div className="w-[340px] stroke-[1px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="342" height="2" viewBox="0 0 342 2" fill="none">
+                        <path d="M1 1H341" stroke="#D9D9D9" stroke-linecap="round" />
+                    </svg>
+                </div>
+                <Topping />
+            </div>
+        </div>
+    )
+};
+
+export default ToppingSelect;
--- src/front-end/src/pages/product.jsx
+++ src/front-end/src/pages/product.jsx
@@ -1,10 +1,21 @@
+import SizeSelect from "../components/sizeSelect";
+import ToppingSelect from "../components/toppingSelect";
 
 
 const Product = () => {
     return (
-        <div className="w-screen h-fit mx-auto opacity-20 bg-black">
-            <div className="w-[900px] h-[690px]">
-
+        <div className="w-screen h-fit mx-auto">
+            <div className="w-screen mx-auto bg-[#F4F2EC] bg-[url('../../public/assets/background.svg')]">
+                <div className="w-[900px] h-[690px] bg-white mx-auto rounded-xl shadow-2xl">
+                    <div className="w-[400px] h-[fit] ml-auto mr-4 rounded-lg border-[#F4F2EC] border mt-auto overflow-hidden">
+                        <div className="w-[400px] h-[100px] mx-auto p-0">
+                            <SizeSelect />
+                        </div>
+                        <div className="w-[400px] h-fit mx-auto -mt-2.5 p-0">
+                            <ToppingSelect />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
