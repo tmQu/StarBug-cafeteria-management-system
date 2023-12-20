f0614b8d75eb0f72f6cf5c152f6d60356d349f0a
HuyBean <nqhuy0711@gmail.com>
2023-12-20 15:22:46 +0700

    Huy - provide user avt component

---
+++ src/front-end/src/components/avatar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Avatar = (props) => {
+  const { onClick, theme } = props;
+  const logoSrc =
+    theme === "dark"
+      ? "./assets/noname.png"
+      : "./assets/noname.png";
+
+  return (
+    <button onClick={onClick}>
+      <div className="flex flex-row items-center justify-center rounded-full">
+        <img
+          className="object-cover rounded-full w-[40px] h-[40px] transition-transform duration-500 ease-in-out hover:scale-[1.02]"
+          alt="logo"
+          src={logoSrc}
+        ></img>
+      </div>
+    </button>
+  );
+};
+
+export default Avatar;
